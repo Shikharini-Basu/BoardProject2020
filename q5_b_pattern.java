7430581ed07360e6e31555a5af81f0916834f7bc
Shikharini Basu <shikharinibasu@gmail.com>
2020-06-02 00:54:19 +0530

    .

---
+++ q5_b_pattern.java
@@ -0,0 +1,31 @@
+/** a b c d e
+    a b c d
+    a b c
+    a b
+    a
+ */
+public class q5_b_pattern
+{
+    int rows; char c='a';
+    void init(int n)
+    {
+        rows=n;
+    }
+    void calc()
+    {
+        for( int i=1;i<=rows;i++)
+        {
+            for( int j=0; j<=(rows-i) ; j++)
+            {
+                System.out.print((char)(c+j)+" ");
+            }
+              System.out.println();
+            }
+        }
+    public static void main (String args[])
+    {
+        q5_b_pattern obj= new q5_b_pattern();
+        obj.init(5);
+        obj.calc();
+    }
+}
