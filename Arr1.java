5a789f545a7eced312c464da51893d67f180c41c
Shikharini Basu <shikharinibasu@gmail.com>
2020-11-17 11:14:50 +0530

    17th nov. 2020

--- Arr1.java
+++ Arr1.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 class Arr1
 { 
-    int[] arr= new int[10];
+    int[] arr= new int[5];
     void init()
     { 
         Scanner sc= new Scanner( System.in);
-        System.out.println(" enter 10 nos.");
+        System.out.println(" enter 5 nos.");
         for(int i=0; i<=arr.length-1; i++)
         { arr[i]=sc.nextInt();
         }
     }
     void display()
-    { for(int i=0; i<=arr.length-1; i++)
-         System.out.println(arr[i]*2);
+    { int total,sum=0; double avg;
+        for(int i=0; i<=arr.length-1; i++)
+         {  sum=sum+arr[i];}
+             total=sum;
+             avg=(double)(total/(arr.length));
+              System.out.println(total+"\n"+avg);
         }
     public static void main()
     { Arr1 obj= new Arr1();
